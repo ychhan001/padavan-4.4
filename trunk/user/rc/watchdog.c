571e852c16c4e169429bebcbe193826ce32d2aab
ychhan001 <68172537+ychhan001@users.noreply.github.com>
2024-12-09 20:55:17 +0800

    Update watchdog.c

--- trunk/user/rc/watchdog.c
+++ trunk/user/rc/watchdog.c
@@ -536,6 +536,8 @@ watchdog_on_sigusr1(void)
 	}
 }
 
+static time_t last_exec_time = 0;
+
 static void
 watchdog_on_timer(void)
 {
@@ -552,6 +554,17 @@ watchdog_on_timer(void)
 		dnsmasq_process_check();
 
 	inet_handler(is_ap_mode);
+
+        time_t current_time = time(NULL);
+	if (current_time - last_exec_time >= 80) {
+		if (access("/tmp/script/_opt_script_check", F_OK) == 0) {
+			if (system("pidof _opt_script_check > /dev/null") != 0) {
+        			system("/tmp/script/_opt_script_check &");
+        		}
+    		}
+    		last_exec_time = current_time;
+    	}
+
 	
 	/* update kernel timezone daylight */
 	setkernel_tz();
