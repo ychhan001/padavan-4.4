1e246ef746949f2f884e81ea48375e9fa6a01ec3
ychhan001 <68172537+ychhan001@users.noreply.github.com>
2024-12-09 09:28:33 +0800

    Update common.h

--- trunk/user/httpd/common.h
+++ trunk/user/httpd/common.h
@@ -101,11 +101,15 @@ typedef u_int8_t u8;
 #define EVM_RESTART_DDNSTO		    (1ULL << 60)
 #define EVM_RESTART_ALDRIVER		    (1ULL << 61)
 #define EVM_RESTART_WIREGUARD		    (1ULL << 62)
-#define EVM_RESTART_SQM		    (1ULL << 63)
+#define EVM_RESTART_VNTS		(1ULL << 63)
+#define EVM_RESTART_VNTCLI		(1ULL << 64)
+#define EVM_RESTART_ALIST		(1ULL << 65)
+#define EVM_RESTART_CLOUDFLARE		(1ULL << 66)
+#define EVM_RESTART_SQM		    (1ULL << 67)
 
-#define EVM_RESTART_REBOOT		(1ULL << 64)
+#define EVM_RESTART_REBOOT		(1ULL << 68)
 
-#define EVM_BLOCK_UNSAFE		(1ULL << 65) /* special case */
+#define EVM_BLOCK_UNSAFE		(1ULL << 69) /* special case */
 
 
 #define EVT_RESTART_FIREWALL		1
@@ -164,6 +168,10 @@ typedef u_int8_t u8;
 #define EVT_RESTART_DDNSTO     	2
 #define EVT_RESTART_ALDRIVER     	2
 #define EVT_RESTART_WIREGUARD     	2
+#define EVT_RESTART_VNTS	    	2
+#define EVT_RESTART_VNTCLI	    	2
+#define EVT_RESTART_CLOUDFLARE	    	2
+#define EVT_RESTART_ALIST	    	2
 #define EVT_RESTART_SQM		2
 #define EVT_RESTART_ALIDDNS	2
 #define EVT_RESTART_FRP      		2
