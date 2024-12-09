261b2b06930d87ed5a9845a004a88aac3834cfb9
ychhan001 <68172537+ychhan001@users.noreply.github.com>
2024-12-09 10:00:35 +0800

    Update rc.c

--- trunk/user/rc/rc.c
+++ trunk/user/rc/rc.c
@@ -1193,6 +1193,30 @@ handle_notifications(void)
 			restart_frp();
 		}
 #endif
+#if defined(APP_VNTS)
+		else if (strcmp(entry->d_name, RCN_RESTART_VNTS) == 0)
+		{
+			restart_vnts();
+		}
+#endif
+#if defined(APP_VNTCLI)
+		else if (strcmp(entry->d_name, RCN_RESTART_VNTCLI) == 0)
+		{
+			restart_vntcli();
+		}
+#endif
+#if defined(APP_ALIST)
+		else if (strcmp(entry->d_name, RCN_RESTART_ALIST) == 0)
+		{
+			restart_alist();
+		}
+#endif
+#if defined(APP_CLOUDFLARE)
+		else if (strcmp(entry->d_name, RCN_RESTART_CLOUDFLARE) == 0)
+		{
+			restart_cloudflare();
+		}
+#endif
 #if defined(APP_DNSFORWARDER)
 		else if (strcmp(entry->d_name, RCN_RESTART_DNSFORWARDER) == 0)
 		{
